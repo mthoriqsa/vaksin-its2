8fe39365cdb499afe400bab6f8d11371e387ad2c
Muhammad Thoriq <mthoriq0001@gmail.com>
2021-08-20 14:17:21 +0700

    Filter pegawai

---
+++ application/controllers/admin/Pegawai.php
@@ -0,0 +1,40 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Pegawai extends CI_Controller {
+
+	public function index()
+	{
+		$query = $this->db->get('pegawai')->result();
+		// print_r($data);
+		// exit();
+		$data = array();
+		$data['result'] = $query;
+		//print_r($data); exit();
+		$this->load->view('admin/pegawai', $data);
+	}
+
+	public function detail(){
+		$data = array();
+		$id = $this->input->post('id_pegawai');
+
+		$data['result'] = $this->db->where('id_pegawai', $id)->get('pegawai')->row();
+		$this->load->view('admin/pegawai_detail', $data);
+	}
+
+	public function filter(){
+		$data = array();
+		$f_jk = $this->input->get('jenis_kelamin');
+		$f_gol = $this->input->get('golongan');
+		$f_status = $this->input->get('status');
+
+
+
+		if(!is_null($f_jk) && $f_jk !== ''){ $this->db->where('jenis_kelamin', $f_jk); }
+		if(!is_null($f_gol) && $f_gol !== ''){ $this->db->where('golongan_darah', $f_gol); }
+		if(!is_null($f_status) && $f_status !== ''){ $this->db->where('status', $f_status); }
+		
+		$data['result'] = $this->db->get('pegawai')->result();
+		$this->load->view('admin/pegawai', $data);
+	}
+}
--- application/views/admin/pegawai.php
+++ application/views/admin/pegawai.php
@@ -4,6 +4,7 @@
     <?php $this->load->view('_partials/head.php') ?>
   </head>
   <body>
+
     <aside class="aside aside-fixed">
       <?php $this->load->view('_partials/sidebar.php') ?>
     </aside>
@@ -19,39 +20,91 @@
               <nav aria-label="breadcrumb" class="d-none d-lg-block">
                 <ol class="breadcrumb breadcrumb-style2 mg-b-10">
                   <li class="breadcrumb-item"><a href="../dashboard">Beranda</a></li>
-                  <li class="breadcrumb-item active" aria-current="page">Jenis Vaksin</li>
+                  <li class="breadcrumb-item active" aria-current="page">Pegawai</li>
                 </ol>
               </nav>
               <h4 class="mg-b-0 tx-montserrat tx-medium text-truncate">
-                Jenis Vaksin
+                Pegawai
               </h4>
             </div>
             <div class="d-lg-none mg-t-10">
             </div>
             <div>
-              <a href="#tambahjenisvaksin" data-toggle="modal" data-animation="effect-scale" class="btn btn-its tx-montserrat tx-semibold"><i data-feather="plus" class="wd-10 mg-r-5 tx-color-its2"></i> Tambah</a>
             </div>
           </div>
 
           <div class="row row-xs">
+            <div class="col-sm-12 col-lg-12 mb-4">
+              <div class="card">
+                <div class="card-body px-3 py-3">
+                  <form method="get" action="<?php echo base_url();?>admin/pegawai/filter/">
+                    <div class="row">
+                      <div class="col-md-3">
+                        <label>Jenis Kelamin</label>
+                          <select class="form-control" id="jenis_kelamin" name="jenis_kelamin">
+                            <option value="">Pilih salah satu</option>
+                            <option value="L">Laki-laki</option>
+                            <option value="P">Perempuan</option>
+                          </select>
+                      </div>
+                      <div class="col-md-3">
+                        <label>Golongan Darah</label>
+                          <select class="form-control" id="golongan" name="golongan">
+                            <option value="">Pilih salah satu</option>
+                            <option value="A">A</option>
+                            <option value="B">B</option>
+                            <option value="O">O</option>
+                            <option value="AB">AB</option>
+                          </select>
+                      </div>
+                      <div class="col-md-3">
+                        <label>Status</label>
+                          <select class="form-control" id="status" name="status">
+                            <option value="">Pilih salah satu</option>
+                            <option value="1">Aktif</option>
+                            <option value="0">Tidak aktif</option>
+                          </select>
+                      </div>
+                      <div class="col-md-3">
+                        <button type="submit" class="btn btn-its tx-montserrat tx-semibold mg-l-5">Filter</button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
             <div class="col-sm-12 col-lg-12">
               <div class="card">
                 <div class="card-body pd-0 table-responsive">
                   <table id="example1" class="table table-borderless mg-0">
                     <thead>
                       <tr class="tx-10 tx-spacing-1 tx-color-03 tx-uppercase">
-                        <th class="wd-80p th-its" style="border-bottom: none !important"><span class="mg-r-15">Nama</span></th>
-                        <th class="wd-20p th-its" style="border-bottom: none !important"><span class="mg-r-15">Aksi</span></th>
+                        <th class="wd-25p th-its" style="border-bottom: none !important"><span>Pegawai</span></th>
+                        <th class="wd-10p th-its" style="border-bottom: none !important"><span>Jenis Kelamin</span></th>
+                        <th class="wd-10p th-its" style="border-bottom: none !important"><span>Tgl Lahir</span></th>
+                        <th class="wd-20p th-its" style="border-bottom: none !important"><span>NIP/NPP</span></th>
+                        <th class="wd-10p th-its" style="border-bottom: none !important"><span>Golongan Darah</span></th>
+                        <th class="wd-15p th-its" style="border-bottom: none !important"><span>Nomor HP</span></th>
+                        <th class="wd-10p th-its" style="border-bottom: none !important"><span>Status</span></th>
                       </tr>
                     </thead>
                     <tbody>
                       <?php foreach ($result as $r) { ?>
-                        <tr>
-                          <td class="td-its align-middle border-bottom"><?php echo $r->nama_vaksin ?></td>
-                          <td class="td-its align-middle border-bottom"><a href="#" class="btn btn-white btn-icon btn-delete" role="button" data-toggle="modal" data-target="#hapusjenisvaksin" data-animation="effect-scale" data-id="<?php echo $r->id_vaksin ?>"><i data-feather="trash" class="wd-10"></i></a></td>
-                        </tr>
+                      <tr>
+                        <td class="td-its align-middle border-bottom">
+                          <a href="<?php echo base_url();?>admin/pegawai/detail?id=<?php $r->id_pegawai ?>">
+                            <p class="mg-b-0 tx-medium tx-color-its3"><?php echo $r->nama_pegawai ?></p>
+                            <p class="mg-b-0 tx-13 tx-color-03"><?php echo $r->nik_pegawai ?></p>
+                          </a>
+                        </td>
+                        <td class="td-its align-middle border-bottom"><?php echo $r->jenis_kelamin ?></td>
+                        <td class="td-its align-middle border-bottom"><?php echo $r->tanggal_lahir ?></td>
+                        <td class="td-its align-middle border-bottom"><?php echo $r->nip_pegawai ?></td>
+                        <td class="td-its align-middle border-bottom"><?php echo $r->golongan_darah ?></td>
+                        <td class="td-its align-middle border-bottom"><?php echo $r->nomor_hp ?></td>
+                        <td class="td-its align-middle border-bottom"><?php echo ($r->status == 1 ? 'Aktif' : 'Tidak Aktif'); ?></td>
+                      </tr>
                       <?php } ?>
-                      
                     </tbody>
                   </table>
                 </div>
@@ -63,20 +116,20 @@
       </div>
     </div>
 
-    <div class="modal fade effect-scale" id="tambahjenisvaksin" tabindex="-1" role="dialog" aria-labelledby="modalmyfile" aria-hidden="true">
+    <div class="modal fade effect-scale" id="tambahvaksinator" tabindex="-1" role="dialog" aria-labelledby="modalmyfile" aria-hidden="true">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-body">
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
-            <h5 class="tx-montserrat tx-medium" id="tambahjenisvaksinLabel">Tambah Jenis Vaksin</h5>
+            <h5 class="tx-montserrat tx-medium" id="tambahvaksinatorLabel">Tambah Vaksinator</h5>
           </div>
-          <form method="post" action="<?php echo base_url();?>admin/jenis_vaksin/add/">
+          <form>
             <div class="modal-body pd-t-0">
               <div class="form-group">
                 <label class="d-block tx-10 tx-spacing-1 tx-color-03 tx-uppercase tx-semibold">Nama</label>
-                <input type="text" id="nama_vac" name="nama_vac" class="form-control" required>
+                <input type="text" id="nama" name="nama" class="form-control" required>
               </div>
             </div>
             <div class="modal-footer">
@@ -84,24 +137,6 @@
               <button type="submit" class="btn btn-its tx-montserrat tx-semibold mg-l-5">Simpan</button>
             </div>
           </form>
-        </div>
-      </div>
-    </div>
-
-    <div class="modal fade effect-scale" id="hapusjenisvaksin" tabindex="-1" role="dialog" aria-labelledby="hapusjenisvaksin" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content tx-14 bg-white">
-          <div class="modal-body">
-            <h5 class="tx-montserrat tx-medium">Apakah Anda yakin ingin menghapus jenis vaksin ini?</h5>
-            <span>Tindakan ini tidak dapat dibatalkan.</span>
-          </div>
-          <div class="modal-footer bd-t-0">
-            <form>
-              <a href="#" data-toggle="modal" data-animation="effect-scale" class="btn btn-white tx-montserrat tx-semibold" data-dismiss="modal">Batalkan</a>
-              <!-- <button type="submit" class="btn btn-its tx-montserrat tx-semibold mg-l-5">Hapus</button> -->
-              <a href="<?php echo base_url();?>admin/jenis_vaksin/delete/" id="link-delete" class="btn btn-its tx-montserrat tx-semibold mg-l-5">Hapus</a>
-            </form>
-          </div>
         </div>
       </div>
     </div>
@@ -185,13 +220,6 @@
         // Select2
         $('.dataTables_length select').select2({ minimumResultsForSearch: Infinity });
 
-      });
-    </script>
-    <script type="text/javascript">
-      $(".btn-delete").click(function(){
-        var id=$(this).data("id");
-        $("#link-delete").attr("href","<?php echo base_url();?>admin/jenis_vaksin/delete?id="+id);
-        console.log(id);
       });
     </script>
 
